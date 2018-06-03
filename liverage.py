3799659883efa36c0453e0ca679e8665266bca05
chiju <chijumelveettil@gmail.com>
2018-06-04 00:18:56 +0530

    will be able to execute as command in the form of liverage STOCK_NAME

--- liverage.py
+++ liverage.py
@@ -1,11 +1,21 @@
-import requests
-from bs4 import BeautifulSoup
-res = requests.get('https://zerodha.com/margin-calculator/Equity/').text
-soup = BeautifulSoup(res, 'html.parser')
+#!/usr/bin/env python3
+
+Usage = """./liverage STOCK_NAME"""
+
 def margin(stock):
     st=stock+':EQ'
+    #print(st)
     for row in soup.tbody.findAll('tr'):
         for td in row.findAll('td'):
             if td.text == st:
                 print(td.text)
                 print(td.findNext(attrs='mis').text.strip())
+if __name__ == "__main__":
+	import requests
+	import sys
+	from bs4 import BeautifulSoup
+	res = requests.get('https://zerodha.com/margin-calculator/Equity/').text
+	soup = BeautifulSoup(res, 'html.parser')
+	stock = sys.argv[1]
+	#print(sys.argv[1])
+	margin(stock)
