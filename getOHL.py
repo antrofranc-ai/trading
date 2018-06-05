617851a7b31adfcab8f1e29046848ac921b9e887
chiju <chijumelveettil@gmail.com>
2018-06-05 20:04:05 +0530

    removed the table border lines and added alligent for the table

--- getOHL.py
+++ getOHL.py
@@ -3,6 +3,11 @@
 import requests
 import texttable as tt
 tab = tt.Texttable()
+tab.set_deco(tab.HEADER)
+tab.set_cols_align(["l", "c", "c"])
+tab.set_cols_dtype(['t',  # text
+                              'f',  # float (decimal)
+                              't']) # text
 from bs4 import BeautifulSoup
 res = requests.get('https://docs.google.com/spreadsheets/d/1rBV78LxZcu2J2L5rCEaBPAujnY4NJEbxLMqsfQXxh0g/pubhtml/sheet?headers=false&gid=0').text
 soup = BeautifulSoup(res, 'html.parser')
@@ -12,5 +17,4 @@ for i in soup.tbody.contents:
     if i.contents[9].text == 'OPEN=HIGH' or i.contents[9].text == 'OPEN=LOW':
         contents = [i.contents[j].text for j in {1,7,9}]
         tab.add_row(contents)
-s = tab.draw()
-print (s)
+print(tab.draw())
