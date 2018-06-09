a8e1aa5743bebbf00aa0dd2f9340f333379ea966
chiju <chijumelveettil@gmail.com>
2018-06-09 21:39:04 +0530

    changed mail content to html

--- mailOHL.py
+++ mailOHL.py
@@ -2,38 +2,45 @@
 
 import requests
 import texttable as tt
+import smtplib
+import email.message
 from bs4 import BeautifulSoup
-tab = tt.Texttable()
-#tab.set_deco(tab.HEADER)
-tab.set_cols_align(["l", "c", "c", "c", "c"])
-tab.set_cols_dtype(['t',  # text
-                    'f',  # float (decimal)
-                    't',  # text
-                    't',  # text
-                    'f']) # float (decimal)
 res = requests.get('https://docs.google.com/spreadsheets/d/1rBV78LxZcu2J2L5rCEaBPAujnY4NJEbxLMqsfQXxh0g/pubhtml/sheet?headers=false&gid=0').text
 res2 = requests.get('https://zerodha.com/margin-calculator/Equity/').text
 soup = BeautifulSoup(res, 'html.parser')
 soup2= BeautifulSoup(res2, 'html.parser')
-headings = [soup.tbody.tr.contents[j].text for j in {1,7,9,5}]
-headings.append('ZERODHA')
-tab.add_row(headings)
+heading = [soup.tbody.tr.contents[j].text for j in {1,7,9,5}]
+heading.append('ZERODHA')
+html = """<html><table border="1">
+            <tr>"""
+for headings in heading:
+    html +="<th>{}</th>".format(headings)
+html +="</tr>"
 for i in soup.tbody.contents:
     if i.contents[9].text == 'OPEN=HIGH' or i.contents[9].text == 'OPEN=LOW':
-        contents = [i.contents[j].text for j in {1,7,9,5}]
-        stock=contents[0]
+        content = [i.contents[j].text for j in {1,7,9,5}]
+        stock=content[0]
         st=stock+':EQ'
         for row in soup2.tbody.findAll('tr'):
             for td in row.findAll('td'):
                 if td.text == st:
-                    contents.append(td.findNext(attrs='mis').text.strip())
-                    tab.add_row(contents)
-import smtplib
+                    content.append(td.findNext(attrs='mis').text.strip())
+        html +="<tr>"
+        for conts in content:
+            html +="<td>{}</td>".format(conts)
+        html +="</tr>"
+html += "</table></html>"
+msg = email.message.Message()
+msg['Subject'] = 'OHL'
+msg['To'] = 'chijumelveettil@gmail.com'
+password = "9567076828Cc#"
+msg['From'] = 'chijumel@gmail.com'
+msg.add_header('Content-Type', 'text/html')
+msg.set_payload(html)
 smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
 smtpserver.ehlo()
 smtpserver.starttls()
 smtpserver.ehlo()
-smtpserver.login("chijumel@gmail.com", "9567076828Cc#")
-message = tab.draw()
-smtpserver.sendmail("chijumel@gmail.com", "chijumelveettil@gmail.com", message)
+smtpserver.login(msg['From'], password)
+smtpserver.sendmail(msg['From'], [msg['To']], msg.as_string())
 smtpserver.quit()
