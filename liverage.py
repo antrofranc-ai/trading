73babf35a5e846ada2546457d34a71876be280a7
chiju <chijumelveettil@gmail.com>
2018-08-13 23:01:08 +0530

    liverage.py will display amount of stocks according to the amount

--- liverage.py
+++ liverage.py
@@ -2,26 +2,27 @@
 
 Usage = """./liverage STOCK_NAME"""
 
-def margin(stock):
-    st=stock+':EQ'
-    #print(st)
-    for row in soup.tbody.findAll('tr'):
-        for td in row.findAll('td'):
-            if td.text == st:
-                print(td.text)
-                print(td.findNext(attrs='mis').text.strip())
+def margin(stock, stock_price):
+	st=stock+':EQ'
+	#print(st)
+	for row in soup.tbody.findAll('tr'):
+		for td in row.findAll('td'):
+			if td.text == st.upper():
+				print(td.text)
+                		#print(td.findNext(attrs='mis').text.strip())
+				liverage=int(td.findNext(attrs='mis').text.strip().strip('x'))
+				print(liverage)
+				amount=31000
+				print(amount)
+				number_of_stocks=(amount*liverage)/stock_price
+				print(number_of_stocks)
 if __name__ == "__main__":
 	import requests
 	import sys
 	from bs4 import BeautifulSoup
 	res = requests.get('https://zerodha.com/margin-calculator/Equity/').text
 	soup = BeautifulSoup(res, 'html.parser')
-	#try:
-	if sys.argv[1]:
-		stock = sys.argv[1]
-	#except IndexError:
-	#	print(Usage)		
+	stock = sys.argv[1]
+	stock_price = int(sys.argv[2])
 	#print(sys.argv[1])
-		margin(stock)
-	else:
-		
+	margin(stock, stock_price)
