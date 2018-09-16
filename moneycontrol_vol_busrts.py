482f7ef327fdf8d833091cd97c5eb320cd0e71db
chiju <chijumelveettil@gmail.com>
2018-09-16 14:31:11 +0530

    added sorting to moneycontrol_vol_busrts.py

--- moneycontrol_vol_busrts.py
+++ moneycontrol_vol_busrts.py
@@ -3,6 +3,7 @@
 from bs4 import BeautifulSoup
 import requests
 from tabulate import tabulate
+import operator
 url_moneycontrol = 'https://m.moneycontrol.com/sensex.php?index=2&id=4'
 res_moneycontrol = requests.get(url_moneycontrol).text
 soup_moneycontrol = BeautifulSoup(res_moneycontrol, 'html.parser')
@@ -16,4 +17,5 @@ for i in soup_moneycontrol.find_all(cellpadding="3"):
                     for td in table:
                         stock_moneycontrol.append(td.text.strip('\xa0\xa0'))
                     volstock_moneycontrol.append(stock_moneycontrol)
-print(tabulate(volstock_moneycontrol))
+table_data = sorted(volstock_moneycontrol, key=operator.itemgetter(int(2)), reverse=True)
+print(tabulate(table_data))
